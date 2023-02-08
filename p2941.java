0b097573ca0960fa460a034aae3de0bb48925267
dlswns2480 <dlswns2480@naver.com>
2023-02-09 03:30:19 +0900

    2941 solved

---
+++ p2941.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class p2941 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("c=");
+        list.add("c-");
+        list.add("dz=");
+        list.add("d-");
+        list.add("lj");
+        list.add("nj");
+        list.add("s=");
+        list.add("z=");
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        String alpha = sc.next();
+        for(int i = 0; i < list.size(); i++){
+            if(alpha.contains(list.get(i))){
+                alpha = alpha.replace(list.get(i), "x");
+            }
+        }
+//        for(int i = 0; i < list.size(); i++){
+//           alpha = alpha.replace(list.get(i), "");
+//        }
+        sum += alpha.length();
+//        System.out.println(alpha);
+        System.out.print(sum);
+    }
+}
