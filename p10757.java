c5e79d0f063e0b18bba82f4a0b8bb1efb6eabf8b
dlswns2480 <dlswns2480@naver.com>
2023-04-30 22:56:12 +0900

    char 형태 대문자 소문자로 변환

--- p10757.java
+++ p10757.java
@@ -1,16 +1,32 @@
 package src;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class p10757 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public String[] solution(String[] players, String[] callings) {
 
-        BigInteger A = sc.nextBigInteger();
-        BigInteger B = sc.nextBigInteger();
-        BigInteger add = B.add(A);
+        HashMap<String, Integer> gi = new HashMap<String, Integer>();
+        String[] answer = new String[players.length];
+        for (int i = 0; i < players.length; i++) {
+            answer[i] = "";
 
-        System.out.print(add);
+        }
+        String temp;
+        for (int i = 0; i < players.length; i++) {
+            //int getIndex = Arrays.asList(players).indexOf(callings[i]);
+            gi.put(players[i], i);
+        }
+        for (String elem:callings) {
+            int getValues = gi.get(elem);
+            if(getValues == 0 )
+                continue;
+            getValues--;
+            answer[getValues] = elem;
+        }
+        return answer;
     }
 }
---
+++ practice.java
@@ -0,0 +1,30 @@
+package src;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class practice {
+    String[] players = {"mumu", "soe", "poe", "kai", "mine"};
+    String[] callings = {"kai", "kai", "mine", "mine"};
+
+    public String[] solution(String[] players, String[] callings) {
+
+        HashMap<String, Integer> gi = new HashMap<String, Integer>();
+
+        String temp;
+        for (int i = 0; i < callings.length; i++) {
+            int getIndex = Arrays.asList(players).indexOf(callings[i]);
+
+            if(getIndex ==0)
+                continue;
+            temp = players[getIndex];
+            players[getIndex] = players[getIndex - 1];
+            players[getIndex - 1] = temp;
+        }
+        return players;
+    }
+
+
+
+}
