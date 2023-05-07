1010a9a4872535ce346b1c2dfca6700ede411234
dlswns2480 <dlswns2480@naver.com>
2023-05-08 00:03:10 +0900

    Solved 2023-05-08

---
+++ CompareNum.java
@@ -0,0 +1,12 @@
+package src;
+
+public class CompareNum {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if(num1 == num2)
+        {
+            return 1;
+        }
+        return -1;
+    }
+}
