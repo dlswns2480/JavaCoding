990bca711007a62f83c4d12617bad7e0e072c16b
dlswns2480 <dlswns2480@naver.com>
2023-06-08 16:34:31 +0900

    Modify XbetweenN

--- Level1/XbetrweenN.java
+++ Level1/XbetrweenN.java
@@ -4,11 +4,11 @@ public class XbetrweenN {
     public long[] solution(long x, int n) {
         long[] answer = {};
         answer = new long[n];
-        if(x == 0){
-            for(int i = 0; i < n;){
-                answer[i] = 0;
-            }
-        }
+//        if(x == 0){
+//            for(int i = 0; i < n;){
+//                answer[i] = 0;
+//            }
+//        }
         answer[0] = x;
         for(int i = 1; i < n; i++){
             answer[i] = x * (i + 1);
